19cf42c55078ce57d755a7daa7c17902e079d43d
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-08-28 13:57:14 +0500

    loan

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import Offer from './Components/Offer/Offer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header/>
+     <Offer/>
       
     </>
   )
---
+++ src/Components/Offer/Offer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import "./Offer.css"
+import loan from "../../Images/real-estate.png"
+import loan1 from "../../Images/home.webp"
+const Offer = () => {
+  return (
+    <>
+     <div className="service_area">
+        <div className='line'></div>
+        <div className='offer-text'>
+            <h2>What we offer for you</h2>
+            <p>We provide online instant cash loans with quick approval that suit your term</p>
+        </div>
+        <div className='cards'>
+            <div className='home'>
+                <div className='home-head'>
+                   <img src={loan} alt='home-loan' className='loan-icon' /> 
+                   <h5>Home Loan</h5>
+                   <h2>$3000-$10000</h2>
+                </div>
+                <div className='home-info'>
+                    <ul>
+                        <li>Borrow - $350 over 3 months</li>
+                        <li>Interest rate - 292% pa fixed</li>
+                        <li>Total amount payable - $525.12</li>
+                        <li>Representative - 1,286% APR</li>
+                    </ul>
+                </div>
+                <div className="ml-20 pt-2 mt-6 ">
+  <a
+    href="apply.html"
+    className="btnn   py-3 px-6 rounded-lg transition duration-300 ease-in-out"
+  >
+    Apply for a Loan
+  </a>
+</div>
+            </div>
+            <div className='car'></div>
+            <div className='education'></div>
+        </div>
+       </div>
+   
+
+    </>
+  )
+}
+
+export default Offer
