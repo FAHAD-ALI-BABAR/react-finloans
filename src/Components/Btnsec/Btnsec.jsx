463475248d60a1624891b168ee7d4f7450cc08c8
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-09-02 15:46:48 +0500

    finloans

--- src/Components/Btnsec/Btnsec.jsx
+++ src/Components/Btnsec/Btnsec.jsx
@@ -18,93 +18,93 @@ const Btnsec = () => {
       <a href="#" className="dis genric-btn disable">Disable</a>
     </div>
 
-    <div className="button-group-area mt-4 space-x-2">
-      <a href="#" className="deff genric-btn default-border">Default</a>
+    <div className="button-group-area mb-4 space-x-2 mt-6">
+      <a href="#" className="deff genric-btn default">Default</a>
       <a href="#" className="orr genric-btn primary-border">Primary</a>
       <a href="#" className="skk genric-btn success-border">Success</a>
-      <a href="#" className="bll genric-btn info-border">Info</a>
+      <a href="#" className="bll genric-btn info-border">Information</a>
       <a href="#" className="yell genric-btn warning-border">Warning</a>
       <a href="#" className="ree genric-btn danger-border">Danger</a>
-      <a href="#" className="genric-btn link-border ">Link</a>
-      <a href="#" className="genric-btn disable">Disable</a>
+      <a href="#" className="deff genric-btn link-border ">Link</a>
+      <a href="#" className="diss genric-btn disable">Disable</a>
     </div>
 
     <div className="button-group-area mt-10 space-x-2">
       <a href="#" className="def genric-btn default radius">Default</a>
-      <a href="#" className="genric-btn primary radius">Primary</a>
-      <a href="#" className="genric-btn success radius">Success</a>
-      <a href="#" className="genric-btn info radius">Info</a>
-      <a href="#" className="genric-btn warning radius">Warning</a>
-      <a href="#" className="genric-btn danger radius">Danger</a>
+      <a href="#" className="or genric-btn primary radius">Primary</a>
+      <a href="#" className="sk genric-btn success radius">Success</a>
+      <a href="#" className="bl genric-btn info radius">Information</a>
+      <a href="#" className="yel genric-btn warning radius">Warning</a>
+      <a href="#" className="re genric-btn danger radius">Danger</a>
       <a href="#" className="def genric-btn link radius">Link</a>
-      <a href="#" className="genric-btn disable radius">Disable</a>
+      <a href="#" className="dis genric-btn disable radius">Disable</a>
     </div>
 
     <div className="button-group-area mt-4 space-x-2">
       <a href="#" className="deff genric-btn default-border radius">Default</a>
       <a href="#" className="orr genric-btn primary-border radius">Primary</a>
       <a href="#" className="skk genric-btn success-border radius">Success</a>
-      <a href="#" className="bll genric-btn info-border radius">Info</a>
+      <a href="#" className="bll genric-btn info-border radius">Information</a>
       <a href="#" className="yell genric-btn warning-border radius">Warning</a>
       <a href="#" className="ree genric-btn danger-border radius">Danger</a>
-      <a href="#" className="genric-btn link-border radius">Link</a>
-      <a href="#" className="genric-btn disable radius">Disable</a>
+      <a href="#" className="deff genric-btn link-border radius">Link</a>
+      <a href="#" className="diss genric-btn disable radius">Disable</a>
     </div>
 
     <div className="button-group-area mt-10 space-x-2">
-      <a href="#" className="def genric-btn default circle">Default</a>
-      <a href="#" className="genric-btn primary circle">Primary</a>
-      <a href="#" className="genric-btn success circle">Success</a>
-      <a href="#" className="genric-btn info circle">Info</a>
-      <a href="#" className="genric-btn warning circle">Warning</a>
-      <a href="#" className="genric-btn danger circle">Danger</a>
-      <a href="#" className="def genric-btn link circle">Link</a>
-      <a href="#" className="genric-btn disable circle">Disable</a>
+    <a href="#" className="def genric-btn default radius">Default</a>
+      <a href="#" className="or genric-btn primary radius">Primary</a>
+      <a href="#" className="sk genric-btn success radius">Success</a>
+      <a href="#" className="bl genric-btn info radius">Information</a>
+      <a href="#" className="yel genric-btn warning radius">Warning</a>
+      <a href="#" className="re genric-btn danger radius">Danger</a>
+      <a href="#" className="def genric-btn link radius">Link</a>
+      <a href="#" className="dis genric-btn disable radius">Disable</a>
     </div>
 
     <div className="button-group-area mt-4 space-x-2">
-      <a href="#" className="deff genric-btn default-border circle">Default</a>
-      <a href="#" className="genric-btn primary-border circle">Primary</a>
-      <a href="#" className="genric-btn success-border circle">Success</a>
-      <a href="#" className="genric-btn info-border circle">Info</a>
-      <a href="#" className="genric-btn warning-border circle">Warning</a>
-      <a href="#" className="genric-btn danger-border circle">Danger</a>
-      <a href="#" className="genric-btn link-border circle">Link</a>
-      <a href="#" className="genric-btn disable circle">Disable</a>
+    <a href="#" className="deff genric-btn default-border radius">Default</a>
+      <a href="#" className="orr genric-btn primary-border radius">Primary</a>
+      <a href="#" className="skk genric-btn success-border radius">Success</a>
+      <a href="#" className="bll genric-btn info-border radius">Information</a>
+      <a href="#" className="yell genric-btn warning-border radius">Warning</a>
+      <a href="#" className="ree genric-btn danger-border radius">Danger</a>
+      <a href="#" className="deff genric-btn link-border radius">Link</a>
+      <a href="#" className="diss genric-btn disable radius">Disable</a>
     </div>
 
     <div className="button-group-area mt-10 space-x-2">
       <a href="#" className="def genric-btn default circle arrow">Default<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn primary circle arrow">Primary<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn success circle arrow">Success<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn info circle arrow">Info<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className=" genric-btn warning circle arrow">Warning<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn danger circle arrow">Danger<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="or-round genric-btn primary circle arrow">Primary<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="sk-round genric-btn success circle arrow">Success<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="bl-round genric-btn info circle arrow">Information<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="yel-round genric-btn warning circle arrow">Warning<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="re-round genric-btn danger circle arrow">Danger<span className="lnr lnr-arrow-right"></span></a>
     </div>
 
     <div className="button-group-area mt-4 space-x-2">
       <a href="#" className="deff genric-btn default-border circle arrow">Default<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn primary-border circle arrow">Primary<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn success-border circle arrow">Success<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn info-border circle arrow">Info<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn warning-border circle arrow">Warning<span className="lnr lnr-arrow-right"></span></a>
-      <a href="#" className="genric-btn danger-border circle arrow">Danger<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="orr-round genric-btn primary-border circle arrow">Primary<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="skk-round genric-btn success-border circle arrow">Success<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="bll-round genric-btn info-border circle arrow">Information<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="yell-round genric-btn warning-border circle arrow">Warning<span className="lnr lnr-arrow-right"></span></a>
+      <a href="#" className="ree-round genric-btn danger-border circle arrow">Danger<span className="lnr lnr-arrow-right"></span></a>
     </div>
 
     <div className="button-group-area mt-10 space-x-2">
-      <a href="#" className="def genric-btn primary e-large">Extra Large</a>
-      <a href="#" className="genric-btn success large">Large</a>
-      <a href="#" className="genric-btn primary">Default</a>
-      <a href="#" className="genric-btn success medium">Medium</a>
-      <a href="#" className="genric-btn primary small">Small</a>
+      <a href="#" className="genric-btn primary e-large bg-orange-900 px-4 py-4 text-white text-lg">Extra Large</a>
+      <a href="#" className="genric-btn success large bg-blue-500 px-3.5 py-3.5 text-white text-base">Large</a>
+      <a href="#" className="genric-btn primary bg-orange-900 px-3 py-2 text-white text-sm">Default</a>
+      <a href="#" className="genric-btn success medium bg-blue-500 px-1 py-1.5 text-white text-xs">Medium</a>
+      <a href="#" className="genric-btn primary small bg-orange-900 px-1 py-1 text-white text-xs">Small</a>
     </div>
 
-    <div className="button-group-area mt-4 space-x-2">
-      <a href="#" className="genric-btn primary-border e-large">Extra Large</a>
-      <a href="#" className="genric-btn success-border large">Large</a>
-      <a href="#" className="genric-btn primary-border">Default</a>
-      <a href="#" className="genric-btn success-border medium">Medium</a>
-      <a href="#" className="genric-btn primary-border small">Small</a>
+    <div className="button-group-area mt-9 space-x-2 ">
+      <a href="#" className="genric-btn primary-border e-large border border-orange-900 px-4 py-4 text-orange-900 text-lg">Extra Large</a>
+      <a href="#" className="genric-btn success-border large border border-blue-500 px-3.5 py-3.5 text-blue-500 text-base">Large</a>
+      <a href="#" className="genric-btn primary-border border border-orange-900 px-3 py-2 text-orange-900 text-sm">Default</a>
+      <a href="#" className="genric-btn success-border medium border border-blue-500 px-1 py-1.5 text-blue-500 text-xs">Medium</a>
+      <a href="#" className="genric-btn primary-border small border border-orange-900 px-1 py-1 text-orange-900 text-xs">Small</a>
     </div>
   </div>
 </section>
